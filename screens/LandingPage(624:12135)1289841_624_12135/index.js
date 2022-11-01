e00bc2f7d52425769fa67e7e5032f1859903d6b6
Crowdbotics <team@crowdbotics.com>
2022-11-01 14:28:01 +0000

    Update components

---
+++ screens/LandingPage(624:12135)1289841_624_12135/index.js
@@ -0,0 +1,227 @@
+import React from "react"
+import {
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Text,
+  Button,
+  Switch,
+  TextInput,
+  StyleSheet,
+  ScrollView
+} from "react-native"
+import Icon from "react-native-vector-icons/FontAwesome"
+import { CheckBox } from "react-native-elements"
+import { connect } from "react-redux"
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp
+} from "react-native-responsive-screen"
+import { getNavigationScreen } from "@screens"
+export class Blank extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {}
+  }
+  render = () => (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      style={styles.ScrollView_1}
+    >
+      <View style={styles.View_2} />
+      <ImageBackground
+        source={{
+          uri:
+            "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/b18e05a7-3135-4360-bfeb-96db3b6507fd"
+        }}
+        style={styles.ImageBackground_624_12136}
+      />
+      <View style={styles.View_624_12137} />
+      <View style={styles.View_624_12138}>
+        <View style={styles.View_I624_12138_119_97}>
+          <Text style={styles.Text_I624_12138_119_97}>4:00</Text>
+        </View>
+        <View style={styles.View_I624_12138_119_98}>
+          <ImageBackground
+            source={{
+              uri:
+                "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/0c75572f-37c7-419e-b89a-2c5e245eeb05"
+            }}
+            style={styles.ImageBackground_I624_12138_119_99}
+          />
+        </View>
+        <View style={styles.View_I624_12138_119_104}>
+          <ImageBackground
+            source={{
+              uri:
+                "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/ecd3010a-0ad5-44f7-8358-b2b2f7112c91"
+            }}
+            style={styles.ImageBackground_I624_12138_119_105}
+          />
+        </View>
+        <View style={styles.View_I624_12138_119_109}>
+          <ImageBackground
+            source={{
+              uri:
+                "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/e9fa1229-c323-4560-92f1-07556352f6c5"
+            }}
+            style={styles.ImageBackground_I624_12138_119_110}
+          />
+        </View>
+      </View>
+      <View style={styles.View_624_12139}>
+        <Text style={styles.Text_624_12139}>Welcome to Daily Budget!</Text>
+      </View>
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  ScrollView_1: { backgroundColor: "rgba(240, 244, 250, 1)" },
+  View_2: { height: hp("115%") },
+  ImageBackground_624_12136: {
+    width: wp("181%"),
+    minWidth: wp("181%"),
+    height: hp("104%"),
+    minHeight: hp("104%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    top: hp("11%"),
+    resizeMode: "cover"
+  },
+  View_624_12137: {
+    width: wp("100%"),
+    minWidth: wp("100%"),
+    height: hp("11%"),
+    minHeight: hp("11%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    top: hp("0%"),
+    backgroundColor: "rgba(27, 33, 36, 1)"
+  },
+  View_624_12138: {
+    width: wp("99%"),
+    minWidth: wp("99%"),
+    height: hp("6%"),
+    minHeight: hp("6%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    top: hp("0%"),
+    backgroundColor: "rgba(0, 0, 0, 0)"
+  },
+  View_I624_12138_119_97: {
+    flexGrow: 1,
+    width: wp("8%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("6%"),
+    top: hp("2%"),
+    justifyContent: "center"
+  },
+  Text_I624_12138_119_97: {
+    color: "rgba(255, 255, 255, 1)",
+    fontSize: 13,
+    fontWeight: "600",
+    textAlign: "left",
+    fontStyle: "normal",
+    letterSpacing: 0.20000000298023224,
+    textTransform: "none"
+  },
+  View_I624_12138_119_98: {
+    flexGrow: 1,
+    width: wp("5%"),
+    height: hp("1%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("75%"),
+    top: hp("2%")
+  },
+  ImageBackground_I624_12138_119_99: {
+    width: wp("5%"),
+    minWidth: wp("5%"),
+    height: hp("1%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    top: hp("0%")
+  },
+  View_I624_12138_119_104: {
+    flexGrow: 1,
+    width: wp("4%"),
+    height: hp("1%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("81%"),
+    top: hp("2%")
+  },
+  ImageBackground_I624_12138_119_105: {
+    width: wp("4%"),
+    minWidth: wp("4%"),
+    height: hp("1%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    top: hp("0%")
+  },
+  View_I624_12138_119_109: {
+    flexGrow: 1,
+    width: wp("7%"),
+    height: hp("2%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("87%"),
+    top: hp("2%")
+  },
+  ImageBackground_I624_12138_119_110: {
+    width: wp("7%"),
+    minWidth: wp("7%"),
+    height: hp("2%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    top: hp("0%")
+  },
+  View_624_12139: {
+    width: wp("71%"),
+    minWidth: wp("71%"),
+    minHeight: hp("5%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("14%"),
+    top: hp("14%"),
+    justifyContent: "center"
+  },
+  Text_624_12139: {
+    color: "rgba(27, 33, 36, 1)",
+    fontSize: 19,
+    fontWeight: "700",
+    textAlign: "left",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    textTransform: "none"
+  }
+})
+
+const mapStateToProps = state => {
+  return {}
+}
+const mapDispatchToProps = () => {
+  return {}
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Blank)
